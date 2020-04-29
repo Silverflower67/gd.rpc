fd3dd479b7b1cfdf223ea9195677eddc33eee585
Nikita Tikhonov <nekitguypro@gmail.com>
2020-04-29 17:10:47 +0300

    [0.1.2] Improve shutdown and interface.

--- gdrpc.py
+++ gdrpc.py
@@ -3,7 +3,7 @@ import datetime
 import gd
 import pypresence
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 def get_timestamp() -> int:
@@ -15,6 +15,7 @@ GD_PROCESS = "GeometryDash.exe"
 LOOP = gd.utils.acquire_loop()
 MESSAGES = {
     gd.memory.Scene.MAIN: "Idle",
+    gd.memory.Scene.SELECT: "Selecting level",
     gd.memory.Scene.EDITOR_OR_LEVEL: "Watching level info",
     gd.memory.Scene.SEARCH: "Searching levels",
     gd.memory.Scene.LEADERBOARD: "Browsing leaderboards",
@@ -48,19 +49,22 @@ def parse_difficulty(level: gd.typing.Optional[gd.Level]) -> str:
 async def main_loop() -> None:
     try:
         memory.reload()
+
     except RuntimeError:
+        await presence.clear()
         return
 
     scene = memory.get_scene()
     best_record = memory.get_normal_percent()
-    level = None
+    editor_object_count = memory.get_object_count()
     level_id = memory.get_level_id()
     level_name = memory.get_level_name()
     level_type = memory.get_level_type()
+    level = None
 
     if level_type == gd.memory.LevelType.NULL:
         if memory.is_in_editor():
-            state = "In Editor"
+            state = f"In Editor ({editor_object_count} objects)"
             details = "Editing level"
 
         else:
@@ -68,7 +72,7 @@ async def main_loop() -> None:
             details = MESSAGES.get(scene, "Unknown")
 
     else:
-        state = f"{level_name} ({best_record}% best) [{level_id}]"
+        state = f"{level_name} ({best_record}% best) [ID: {level_id}]"
 
         if level_type == gd.memory.LevelType.OFFICIAL:
             level = gd.Level.official(level_id, client=client)
@@ -83,6 +87,7 @@ async def main_loop() -> None:
                 level = await client.get_level(level_id, get_data=False)
             except gd.ClientException:
                 pass
+
             details = "Playing online level"
 
     difficulty = parse_difficulty(level)
@@ -95,22 +100,24 @@ async def main_loop() -> None:
         large_image="gd",
         large_text="gd.rpc",
         small_image=difficulty,
+        small_text=difficulty,
     )
 
 
-async def start():
+async def connect() -> None:
     await presence.connect()
 
 
-def run():
+def run() -> None:
 
-    LOOP.run_until_complete(start())
+    LOOP.run_until_complete(connect())
 
     main_loop.start()
 
     try:
         LOOP.run_forever()
     except KeyboardInterrupt:
+        gd.utils.cancel_all_tasks(LOOP)
         presence.close()
 
 
